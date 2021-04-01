03b8c8ee3fbe9b0afc53a5fff1c0cec92badb3b8
BrunaLopesGois <bruna-chan@live.com>
2021-04-01 20:24:26 -0300

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+      <div v-for="(book, id) of books" :key="id">
+        <img :src="`http://localhost:8000/${book.cover}`" alt="" width="100px" height="100px">
+        <ul id="list-books">
+          <li>{{ book.title }}</li>
+          <li>{{ book.genre }}</li>
+          <li>{{ book.description }}</li>
+          <li>{{ book.sale_price }}</li>
+        </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  data() {
+    return {
+      books: [],
+    }
+  },
+
+  created() {
+    this.$http.get('http://localhost:8000/api/books')
+    .then(res => this.books = res.data)
+  }
+
+}
+</script>
+
+<style>
+
+</style>
