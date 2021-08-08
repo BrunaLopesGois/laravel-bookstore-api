6c0fb2d288405c3e8091a94c8ce3fbe7a5fb9808
Bruna Lopes <bruna-chan@live.com>
2021-08-08 16:02:55 -0300

    auth using service provider

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,6 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'jwt.auth' => \App\Http\Middleware\JWTAuth::class
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
     ];
 }
