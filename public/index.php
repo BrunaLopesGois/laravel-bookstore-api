2a95a8c2c0db308d9a50c73dbad88a83040fa7f4
Bruna Lopes <bruna-chan@live.com>
2021-09-07 16:04:27 -0300

    Revert "cors wip"
    This reverts commit 49725315d3531b60bb78173e0492835108994fd1.

--- public/index.php
+++ public/index.php
@@ -58,3 +58,19 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+
+$allowedOrigins = array(
+    '(http(s)://)?(www\.)?laravel-bookstore-api.herokuapp.com', // Laravel API Domain
+    'https://livrariabc.herokuapp.com' // VueJS CLient
+ );
+ if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+    foreach ($allowedOrigins as $allowedOrigin) {
+       if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+          header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+          header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+          header('Access-Control-Max-Age: 1000');
+          header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+          break;
+       }
+    }
+ }
--- routes/api.php
+++ routes/api.php
@@ -18,24 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'cors'], function () {
-    // Rotas de visuação de produto, sem necessidade de autenticação
-    Route::prefix('/books')->group(function () {
-        Route::get('/', [BookController::class, 'index']);
-        Route::get('/{id}', [BookController::class, 'show']);
-    });
-
-    // Rotas autenticadas
-    Route::middleware('jwt.auth')->group(function () {
-        Route::get('/user', [UserController::class, 'show']);
-        Route::prefix('/books')->group(function () {
-            Route::post('/', [BookController::class, 'store']);
-            Route::delete('/{id}', [BookController::class, 'destroy']);
-            Route::put('/{id}', [BookController::class, 'update']);
-        });
-        Route::post('/checkout/books/{id}', [CheckoutController::class, 'checkout']);
-    });
-
-    // Rotas de autenticação
-    Route::post('/login', [AuthController::class, 'signOn']);
+// Rotas de visuação de produto, sem necessidade de autenticação
+Route::prefix('/books')->group(function () {
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/{id}', [BookController::class, 'show']);
 });
+
+// Rotas autenticadas
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('/user', [UserController::class, 'show']);
+    Route::prefix('/books')->group(function () {
+        Route::post('/', [BookController::class, 'store']);
+        Route::delete('/{id}', [BookController::class, 'destroy']);
+        Route::put('/{id}', [BookController::class, 'update']);
+    });
+    Route::post('/checkout/books/{id}', [CheckoutController::class, 'checkout']);
+});
+
+// Rotas de autenticação
+Route::post('/login', [AuthController::class, 'signOn']);
